c1003aa020ce711fe339b320506da2c3216dce05
omarfarah123 <76454677+omarfarah123@users.noreply.github.com>
2022-10-25 00:33:33 -0400

    Created thoughtRoute

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    deleteThought,
+    updateThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController.js');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+    .delete(removeReaction)
+
+module.exports = router
